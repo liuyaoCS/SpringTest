df558a56da6b1138cd975ca57fc0f4355416c2c4
liuyaoCS <liuyao@chinaso.com>
2016-07-08 09:07:59 +0800

    servlet

---
+++ src/cn/lv/servlet/SpringServlet.java
@@ -0,0 +1,32 @@
+package cn.lv.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.ly.bean.HelloService;
+
+public class SpringServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		//WebApplicationContext applicationContext=WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		HelloService helloService=(HelloService) applicationContext.getBean("HelloService");
+		helloService.sayHello();
+	}
+
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+}
--- src/cn/ly/bean/LifeBeanImpl.java
+++ src/cn/ly/bean/LifeBeanImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 public class LifeBeanImpl implements LifeBean,BeanNameAware,ApplicationContextAware{
 	private String name;
